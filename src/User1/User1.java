8fea1c006f2fd7eee09ad4047934bb6ade4777fe
KadamHemangi <hemangi.kadam@hicare.in>
2023-03-31 12:37:04 +0530

    first commit by user 1

---
+++ src/User1/User1.java
@@ -0,0 +1,13 @@
+package User1;
+
+public class User1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Change 1 done by User 1");
+		System.out.println("Change 2 done by User 1");
+
+	}
+
+}
